59501b471c5a1bd49c92f9729fa0667ddc9b5a97
taylor <taylorjrsmith@gmail.com>
2020-01-21 16:38:13 +0000

    Added the initial implementation commit

---
+++ BaseClasses/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicRouting.Kentico.Wildcards.BaseClasses
+{
+    public class Constants
+    {
+
+
+
+    }
+}
